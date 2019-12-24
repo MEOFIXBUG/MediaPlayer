b9ce1f26b7f80fd4bf3fa7a7d0f36d54c02d3f82
Vũ Phạm Đức Thắng <1712763@student.hcmus.edu.vn>
2019-12-24 22:26:55 +0700

    + hiển thị thời gian và slider

--- Player/VIPMP3/ViewModel/MainViewModel.cs
+++ Player/VIPMP3/ViewModel/MainViewModel.cs
@@ -4,10 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 using VIPMP3.Model;
 
 namespace VIPMP3.ViewModel
@@ -19,9 +21,12 @@ namespace VIPMP3.ViewModel
         #region MediaPlayer
         MediaPlayer _mediaPlayer;
         bool _isPlaying = false;
+        public delegate void timerTick();
+        DispatcherTimer ticks = new DispatcherTimer();
+        timerTick tick;
         #endregion
         #region Play_PauseButton
-        public PackIconKind IconKind 
+        public PackIconKind IconKind
         {
             get { return _IconKind; }
             set { _IconKind = value; OnPropertyChanged("IconKind"); }
@@ -41,7 +46,7 @@ namespace VIPMP3.ViewModel
         {
             //initial
             //init media player
-             _mediaPlayer = new MediaPlayer();
+            _mediaPlayer = new MediaPlayer();
             IconKind = MaterialDesignThemes.Wpf.PackIconKind.Play;
             _listPlayingMusics = new ObservableCollection<Music>();
         }
@@ -81,23 +86,76 @@ namespace VIPMP3.ViewModel
             if (dialog.ShowDialog() == true)
             {
                 _mediaPlayer.Open(new Uri(dialog.FileName));
-                _mediaPlayer.Play();
+                StartMusic();
 
                 Music music = new Music();
                 music.Name = dialog.SafeFileName;
                 music.Path = dialog.FileName;
                 music.Cover = null;
-                while(!_mediaPlayer.NaturalDuration.HasTimeSpan)
+                while (!_mediaPlayer.NaturalDuration.HasTimeSpan)
                 {
 
                 }
                 music.Duration = _mediaPlayer.NaturalDuration.TimeSpan;
+                string len = "";
+                if (music.Duration.Minutes < 10)
+                {
+                    len += $"0{music.Duration.Minutes}";
+                }
+                else
+                {
+                    len += $"{music.Duration.Minutes}";
+                }
+                len += ":";
+                if (music.Duration.Seconds < 10)
+                {
+                    len += $"0{music.Duration.Seconds}";
+                }
+                else
+                {
+                    len += $"{music.Duration.Seconds}";
+                }
+                LengthMusic = len;
                 _listPlayingMusics.Add(music);
 
-                _isPlaying = true;
-                IconKind = MaterialDesignThemes.Wpf.PackIconKind.Pause;
-                OnPropertyChanged();
             }
+        }
+        private void StartMusic()
+        {
+            _mediaPlayer.Play();
+            _isPlaying = true;
+            IconKind = MaterialDesignThemes.Wpf.PackIconKind.Pause;
+            ticks.Interval = TimeSpan.FromMilliseconds(1);
+            ticks.Tick += ticks_Tick;
+            ticks.Start();
+            OnPropertyChanged();
+        }
+        void ticks_Tick(object sender, object e)
+        {
+            DurationValue = (int)(_mediaPlayer.Position.TotalMilliseconds / _mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds * 1000);
+            string curTime = "";
+            if (_mediaPlayer.Position.Minutes < 10)
+            {
+                curTime += $"0{_mediaPlayer.Position.Minutes}";
+            } else
+            {
+                curTime += $"{_mediaPlayer.Position.Minutes}";
+            }
+            curTime += ":";
+            if (_mediaPlayer.Position.Seconds < 10)
+            {
+                curTime += $"0{_mediaPlayer.Position.Seconds}";
+            }
+            else
+            {
+                curTime += $"{_mediaPlayer.Position.Seconds}";
+            }
+            CurPosition = curTime;
+            Debug.WriteLine($"Time: {DurationValue}");
+        }
+        void changeStatus()
+        {
+            /* If you want the Slider to Update Regularly Just UnComment the Line Below*/
         }
         #endregion
         #region CloseCommand
@@ -120,6 +178,28 @@ namespace VIPMP3.ViewModel
         private void ExecuteCloseCommand()
         {
             Application.Current.Shutdown();
+        }
+        #endregion
+
+        #region MinimizeCommand
+        private ICommand _minimizeCommand;
+        public ICommand MinimizeCommand
+        {
+            get
+            {
+                return _minimizeCommand ??
+                     (_minimizeCommand = new RelayCommand<object>(
+                         (p) => CanExecuteMinimizeCommand(),
+                         (p) => ExecuteMinimizeCommand()));
+            }
+        }
+        private bool CanExecuteMinimizeCommand()
+        {
+            return true;
+        }
+        private void ExecuteMinimizeCommand()
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
         }
         #endregion
         #region Play_PauseCommand
@@ -150,13 +230,51 @@ namespace VIPMP3.ViewModel
             {
                 _mediaPlayer.Pause();
                 IconKind = MaterialDesignThemes.Wpf.PackIconKind.Play;
-            } else
+            }
+            else
             {
                 _mediaPlayer.Play();
                 IconKind = MaterialDesignThemes.Wpf.PackIconKind.Pause;
             }
             _isPlaying = !_isPlaying;
             OnPropertyChanged();
+        }
+        #endregion
+
+        #region UpdateDuration
+        private int _value;
+        public int DurationValue
+        {
+            get { return _value; } 
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region CurrentTime
+        private string _curTime = "00:00";
+        public string CurPosition
+        {
+            get { return _curTime; }
+            set
+            {
+                _curTime = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region LengthMusic
+        private string lengthMusic;
+        public string LengthMusic
+        {
+            get { return lengthMusic; }
+            set
+            {
+                lengthMusic = value;
+                OnPropertyChanged();
+            }
         }
         #endregion
         #endregion
